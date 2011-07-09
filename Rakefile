65d7f12fe035b007e6a962f8df6baf9de28457a4
Tomokazu Sato <s-tomo@s-tomo.jp>
2011-07-09 15:15:26 +0900

    export script
    delete other objects -> apply modifiers -> export COLLADA

--- Rakefile
+++ Rakefile
@@ -6,6 +6,8 @@ BLENDER = 'D:\Program Files\Blender Foundation\Blender\blender.exe'
 
 CLEAN.add('out/')
 
-task :default do |t|
-  system("#{BLENDER} --help")
+task :default => 'out/unrea_body.dae'
+
+file 'out/unrea_body.dae' => ['unrea.blend', 'export.py'] do |t|
+  system("#{BLENDER} #{t.prerequisites[0]} --python #{t.prerequisites[1]} -- #{t.name}")
 end
---
+++ export.py
@@ -0,0 +1,23 @@
+import bpy.ops
+import sys
+import os
+
+filepath = sys.argv[-1]
+name = os.path.basename(filepath).replace(".dae", "")
+
+if bpy.context.active_object:
+    if bpy.context.active_object.mode == "EDIT":
+        bpy.ops.object.editmode_toggle()
+
+scene = bpy.context.scene
+
+for obj in scene.objects:
+    if obj.name != name:
+        scene.objects.unlink(obj)
+
+bpy.ops.object.select_all(action="SELECT")
+for mod in bpy.context.active_object.modifiers:
+    bpy.ops.object.modifier_apply(modifier=mod.name)
+
+bpy.ops.wm.collada_export(filepath=filepath)
+bpy.ops.wm.quit_blender()
