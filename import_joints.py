541937ff70bbe6cbc3efa634d690fe0a82484097
Tomokazu Sato <s-tomo@s-tomo.jp>
2011-07-29 02:30:36 +0900

    .

--- import_joints.py
+++ import_joints.py
@@ -4,8 +4,11 @@ import math
 
 CHARACTER_DIR = "/Program Files (x86)/SecondLifeViewer2/character/"
 
-def get_vector(elem, attr):    
-    a = elem.get(attr).split(" ")
+def get_vector(elem, attr):
+    v = elem.get(attr)    
+    if v is None:
+        return None
+    a = v.split(" ")
     return list(map(float, a))
 
 def build_bones(armature, parent, parent_bone):
@@ -36,12 +39,9 @@ def import_skeleton():
     bpy.ops.object.mode_set(mode="OBJECT")
     return armature_ob
 
-def import_param(armature_ob, param, z):
-    skeleton = param.find("param_skeleton")
-    if skeleton is None:
-        return
-    
+def create_param_empty(param, z):
     name = param.get("name")
+    print(name)
     armature = armature_ob.data
     empty = bpy.data.objects.new(name, None)
     bpy.context.scene.objects.link(empty)
@@ -68,6 +68,62 @@ def import_param(armature_ob, param, z):
     label_ob.location = (0, -0.5, z)
     label_ob.scale = (0.05, 0.05, 0.05)
     label_ob.rotation_euler = (math.pi/2, 0, math.pi/2)
+    
+    return empty
+
+def add_scale_constraint(scale, value_min, value_max, bone, empty):
+    c = bone.constraints.new("TRANSFORM")
+    c = bone.constraints.new("TRANSFORM")
+    c.from_min_y = 0
+    c.from_max_y = 1
+    c.map_from = "LOCATION"
+    c.map_to = "SCALE"
+    c.map_to_x_from = "Y"
+    c.map_to_y_from = "Y"
+    c.map_to_z_from = "Y"
+    c.target = empty
+    c.to_min_x = 1.0 + scale[0] * value_min
+    c.to_max_x = 1.0 + scale[0] * value_max
+    c.to_min_y = 1.0 + scale[1] * value_min
+    c.to_max_y = 1.0 + scale[1] * value_max
+    c.to_min_z = 1.0 + scale[2] * value_min
+    c.to_max_z = 1.0 + scale[2] * value_max
+    # TODO impl
+
+def add_offset_constraint(offset, value_min, value_max, bone, empty):
+    c = bone.constraints.new("TRANSFORM")
+    c.from_min_y = 0
+    c.from_max_y = 1
+    c.map_from = "LOCATION"
+    c.map_to = "LOCATION"
+    c.map_to_x_from = "Y"
+    c.map_to_y_from = "Y"
+    c.map_to_z_from = "Y"
+    c.target = empty
+    # TODO impl
+
+def import_param(armature_ob, param, z):
+    skeleton = param.find("param_skeleton")
+    if skeleton is None:
+        return
+    
+    value_min = float(param.get("value_min"))
+    value_max = float(param.get("value_max"))
+    
+    empty = create_param_empty(param, z)
+    bpy.ops.object.select_name(name=armature_ob.name)
+    bpy.ops.object.mode_set(mode="POSE")
+    for elem in skeleton.findall("bone"):
+        name = elem.get("name")
+        bone = armature_ob.pose.bones[name]
+        scale = get_vector(elem, "scale")
+        if scale:
+            add_scale_constraint(scale, value_min, value_max, bone, empty)
+        offset = get_vector(elem, "offset")
+        if offset:
+            add_offset_constraint(offset, value_min, value_max, bone, empty)
+        
+    bpy.ops.object.mode_set(mode="OBJECT")
 
 def import_lad(armature_ob):
     doc = xml.etree.ElementTree.parse(CHARACTER_DIR + "avatar_lad.xml")
@@ -80,3 +136,4 @@ def import_lad(armature_ob):
         
 armature_ob = import_skeleton()
 import_lad(armature_ob)
+
