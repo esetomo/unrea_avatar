3065fb7ab4be0861b5ae4d18479831df146c1f8b
Tomokazu Sato <s-tomo@s-tomo.jp>
2011-08-02 01:34:54 +0900

    .

--- export.py
+++ export.py
@@ -2,31 +2,32 @@ import bpy.ops
 import sys
 import os
 
-def modify_armature():
-    bpy.ops.object.mode_set(mode="EDIT")
-    armature = bpy.context.active_object.data
-    for bone in armature.edit_bones:
-        bone.use_connect = False
-    for bone in armature.edit_bones:
-        bone.tail = (bone.head.x, bone.head.y + 0.1, bone.head.z)
-        bone.roll = 0
-    bpy.ops.object.mode_set(mode="OBJECT")
-
 filepath = sys.argv[-1]
 name = os.path.basename(filepath).replace(".dae", "")
 
 bpy.ops.object.mode_set(mode="OBJECT")
 
+bpy.ops.object.select_name(name="avatar")
+bpy.ops.object.mode_set(mode="POSE")
+bpy.ops.pose.select_all(action="SELECT")
+bpy.ops.poselib.apply_pose()
+bpy.ops.object.mode_set(mode="OBJECT")
+
 bpy.ops.object.select_name(name=name)
-# bpy.ops.object.vertex_group_normalize_all(lock_active=False)
 
 for mod in bpy.context.active_object.modifiers:
-    if isinstance(mod, bpy.types.ArmatureModifier):
-        bpy.ops.object.select_name(name=mod.object.name)
-        modify_armature()
-        bpy.ops.object.select_name(name=name,extend=True)
-    else:
-        bpy.ops.object.modifier_apply(modifier=mod.name)
+    bpy.ops.object.modifier_apply(modifier=mod.name)
+
+if bpy.context.active_object.parent:
+    bpy.ops.object.select_name(name="avatar")
+    bpy.ops.object.mode_set(mode="POSE")
+    bpy.ops.pose.select_all(action="SELECT")
+    bpy.ops.pose.armature_apply()
+    bpy.ops.object.mode_set(mode="OBJECT")
+    bpy.ops.object.select_name(name=name)
+    bpy.ops.object.select_name(name="avatar", extend=True)
+    bpy.ops.object.parent_clear(type="CLEAR")
+    bpy.ops.object.parent_set(type="ARMATURE")
 
 bpy.ops.wm.collada_export(filepath=filepath,selected=True)
 bpy.ops.wm.quit_blender()
